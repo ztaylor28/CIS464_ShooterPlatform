65b010a4d4e889315e70207a82fb8a495edb8b7e
Zach Taylor <taylorzachary1998@gmail.com>
2023-03-11 12:00:02 -0500

    Functioning Settings Menu

--- Assets/Main Scripts/SettingsMenu.cs
+++ Assets/Main Scripts/SettingsMenu.cs
@@ -2,11 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
+
+    public AudioMixer audioMixer;
     public void BackToMain()
     {
         SceneManager.LoadScene("MainMenu"); 
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
