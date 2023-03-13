c4d5b1e04382b22463c2eb1e507e6623154a29fa
Zach Taylor <taylorzachary1998@gmail.com>
2023-03-13 12:24:23 -0400

    I added the Intro screen lol.

---
+++ Assets/Main Scripts/IntroCameraMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroCameraMovement : MonoBehaviour
+{
+    [SerializeField] float cameraSpeed; 
+    [SerializeField] Transform targetCameraPosition;
+
+    float cameraPace;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraMovement();
+        TargetReached();
+    }
+
+    void CameraMovement()
+    {
+        cameraPace = cameraSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, targetCameraPosition.position, cameraPace);
+    }
+
+    void TargetReached()
+    {
+        if(transform.position == targetCameraPosition.position)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
